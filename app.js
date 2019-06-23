524a8e92d742700f25ac116c82cd04e6b6837d8a
yu xin <yuxin@guazi.com>
2019-06-23 21:49:20 +0800

    rx-dht-sensor

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-let sensor = require('node-dht-sensor');
+var DHTSeries = require('rx-dht-sensor');
 
-setInterval(() => {
-    sensor.read(11, 4, function(err, temperature, humidity) {
-        if (!err) {
-            console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-                'humidity: ' + humidity.toFixed(1) + '%'
-            );
-        }
-    });
-}, 2000)
+// 创建驱动实例
+var dht11 = new DHTSeries({
+    model: 'dht11',
+    address: 4
+});
+
+// 简单的使用 fetch 方法读取传感器的值，如读取失败，err 参数不为 null
+dht11.fetch(function(err, temperature, humidity) {
+    if (err) {
+        return console.error('a error occur when read dht-sensor:', err);
+    }
+    console.log('temperature: ' + temperature.toFixed(1) + ', humidity: ' + humidity.toFixed(1));
+});
