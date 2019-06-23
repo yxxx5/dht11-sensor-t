d16cd8c3509f068c2b619c1f59ae882593086b8b
yu xin <yuxin@guazi.com>
2019-06-23 19:52:46 +0800

    f

---
+++ app.js
@@ -0,0 +1,9 @@
+let sensor = require('node-dht-sensor');
+
+sensor.read(11, 4, function(err, temperature, humidity) {
+    if (!err) {
+        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+            'humidity: ' + humidity.toFixed(1) + '%'
+        );
+    }
+});
